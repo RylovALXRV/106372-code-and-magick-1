491ad9182eefa703892ec7d79829eeb4b72cc6a5
Алексей <rylovalex2108l@gmail.com>
2018-09-03 13:35:45 +0700

    пример исправленного варианта по лекции №2 задания №10 Учебный проект «Холст»

--- js/stat.js
+++ js/stat.js
@@ -8,7 +8,11 @@ var Bar = {
 };
 
 var Cloud = {
+  DEVIATION: 20,
   HEIGHT: 270,
+  WIDTH: 420,
+  WIDTH_LINE_X: 300,
+  WIDTH_LINE_Y: 190,
   X: 100,
   Y: 10
 };
@@ -17,29 +21,33 @@ var BAR_LEFT_BEGIN = Cloud.X + Bar.WIDTH_BETWEEN_BARS;
 var COLOR_BLACK = '#000000';
 var WIDTH_BAR_AND_TEXT = Bar.WIDTH + Bar.WIDTH_BETWEEN_BARS;
 
-var colors = [];
-
-var getColor = function () {
-  var val = (Math.round(Math.random() * 10)) / 10;
-  if (val === 0) {
-    val = 0.1;
-  }
-  return 'rgba(0, 0, 255, ' + val + ')';
-};
+var values = [];
 
 /*
-* Функция для проверки и получения не похожего цвета у колонок
+* Функция для получения случайного значения от min до max включительно до десятого значения,
+* если такое нужно
 * */
-var checkAndGetNotSameColor = function () {
-  var color = getColor();
-  while (colors) {
-    if (!~colors.indexOf(color)) {
-      colors.push(color);
+function getRandomValue(min, max) {
+  return (Math.random() * (max - min) + min).toFixed(1);
+}
+
+/*
+* Функция для получения не одинакового значения
+* */
+var getNotSameValue = function () {
+  var value = getRandomValue(0.1, 1);
+  while (values) {
+    if (!~values.indexOf(value)) {
+      values.push(value);
       break;
     }
-    color = getColor();
+    value = getRandomValue(0.1, 1);
   }
-  return color;
+  return value;
+};
+
+var getColor = function () {
+  return 'rgba(0, 0, 255, ' + getNotSameValue() + ')';
 };
 
 var displayTextCloud = function (ctx, text, x, y) {
@@ -56,28 +64,49 @@ var getMaxTime = function (arr) {
   return Math.max.apply(null, arr);
 };
 
-var getCloud = function (ctx, x, y, color) {
+var drawCloud = function (ctx, x, y, color) {
   // координаты X и Y с учетом всех точек расположенных на плоскостях
+
   var cords = [
-    {x: x, y: y},
-    {x: 300, y: 20},
-    {x: 120, y: -20},
-    {x: -20, y: 190},
-    {x: 20, y: 80},
-    {x: -300, y: -20},
-    {x: -120, y: 20},
-    {x: 20, y: -190},
-    {x: -20, y: -80}];
-  var cordX = 0;
-  var cordY = 0;
+    {
+      x: x, // 100
+      y: y // 10
+    },
+    {
+      x: x + Cloud.WIDTH_LINE_X, // 100 + 300
+      y: y + Cloud.DEVIATION // 10 + 20
+    },
+    {
+      x: x + Cloud.WIDTH, // 100 + 420
+      y: y // 10
+    },
+    {
+      x: x + Cloud.WIDTH - Cloud.DEVIATION, // 100 + 420 - 20
+      y: y + Cloud.WIDTH_LINE_Y // 10 + 190
+    },
+    {
+      x: x + Cloud.WIDTH, // 100 + 420
+      y: y + Cloud.HEIGHT // 10 + 270
+    },
+    {
+      x: x + Cloud.WIDTH - Cloud.WIDTH_LINE_X, // 100 + 420 - 300
+      y: y + Cloud.HEIGHT - Cloud.DEVIATION // 10 + 270 - 20
+    },
+    {
+      x: x, // 100
+      y: y + Cloud.HEIGHT // 10 + 270
+    },
+    {
+      x: x + Cloud.DEVIATION, // 100 + 20
+      y: y + Cloud.HEIGHT - Cloud.WIDTH_LINE_Y // 10 + 270 - 190
+    }
+  ];
 
   ctx.fillStyle = color;
   ctx.beginPath();
   ctx.moveTo(x, y);
   for (var i = 0; i < cords.length; i++) {
-    cordX += cords[i].x;
-    cordY += cords[i].y;
-    ctx.lineTo(cordX, cordY);
+    ctx.lineTo(cords[i].x, cords[i].y);
   }
   ctx.closePath();
   ctx.fill();
@@ -89,7 +118,7 @@ var renderStatsBar = function (ctx, data, cords) {
   if (data.name === 'Вы') {
     ctx.fillStyle = 'rgba(255, 0, 0, 1)';
   } else {
-    ctx.fillStyle = checkAndGetNotSameColor();
+    ctx.fillStyle = getColor();
   }
   ctx.fillRect(cords.x, cords.y - Bar.GAP * 4, Bar.WIDTH, -data.timePercentage);
   ctx.fillStyle = COLOR_BLACK;
@@ -100,8 +129,8 @@ window.renderStatistics = function (ctx, names, times) {
   // вызываю функцию с максимальным временем и записываю в переменную
   var maxTime = getMaxTime(times);
 
-  getCloud(ctx, Cloud.X + Bar.GAP, Cloud.Y + Bar.GAP, 'rgba(0, 0, 0, 0.7)');
-  getCloud(ctx, Cloud.X, Cloud.Y, '#fff');
+  drawCloud(ctx, Cloud.X + Bar.GAP, Cloud.Y + Bar.GAP, 'rgba(0, 0, 0, 0.7)');
+  drawCloud(ctx, Cloud.X, Cloud.Y, '#fff');
 
   displayTextCloud(ctx, 'Ура вы победили!', Cloud.X + Bar.GAP * 2, Cloud.Y + Bar.GAP + Bar.GAP / 2);
   displayTextCloud(ctx, 'Список результатов:', Cloud.X + Bar.GAP * 2, Cloud.Y + Bar.GAP * 3 + Bar.GAP / 2);
@@ -112,5 +141,5 @@ window.renderStatistics = function (ctx, names, times) {
     renderStatsBar(ctx, {name: names[i], time: times[i], timePercentage: Math.round(times[i] * Bar.MAX_HEIGHT / maxTime)}, {x: BAR_LEFT_BEGIN + WIDTH_BAR_AND_TEXT * i, y: Cloud.HEIGHT});
   }
   // после получения нужных цветов для колонок - обнуляю массив
-  colors = [];
+  values = [];
 };
